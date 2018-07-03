9726e0b3aa990e9e9ab15ce31497f1650facbb19
seto <seto@catchup.co.jp>
2018-07-04 05:14:40 +0900

    設定更新

--- start.sh
+++ start.sh
@@ -3,16 +3,19 @@
 # mysql
 if [ ! -d /var/lib/mysql/mysql ]; then
   su mysql -c "mysqld --initialize-insecure" -s /bin/sh
+  exist_mysql=1
 fi
 
 rm -f /var/lib/mysql/mysql.sock
 rm -f /var/lib/mysql/mysql.sock.lock
 
 su mysql -c "mysqld &" -s /bin/sh
-sleep 10s
-mysql -u root -e "CREATE USER ${MYSQL_USER} IDENTIFIED BY '${MYSQL_PASSWORD}'"
-mysql -u root -e "grant all privileges on *.* to ${MYSQL_USER}@\"%\""
-mysql -u root -e "SET GLOBAL sql_mode = ''"
+if [ $exist_mysql = 1 ]; then
+  sleep 5s
+  mysql -u root -e "CREATE USER ${MYSQL_USER} IDENTIFIED BY '${MYSQL_PASSWORD}'"
+  mysql -u root -e "grant all privileges on *.* to ${MYSQL_USER}@\"%\""
+  mysql -u root -e "SET GLOBAL sql_mode = ''"
+fi
 
 # apache
 rm -f /etc/httpd/conf.d/welcome.conf
